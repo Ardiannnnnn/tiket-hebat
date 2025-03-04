b73e01cfa6bd9de0c0d73c26ebf14dd7b2908ca2
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-03-04 20:25:55 +0700

    add footer navbar

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Mouse_Memoirs } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+
+export const mouseMemoirs = Mouse_Memoirs({ 
+    weight: ['400'],
+    subsets: ['latin']
+});
+
+export const montserrat = Montserrat({
+    weight: ['400', '500', '600'],
+    subsets: ['latin']
+});
---
+++ app/ui/footer.tsx
@@ -0,0 +1,9 @@
+import { mouseMemoirs } from "./fonts"
+
+export default function Footer() {
+    return (
+        <footer className={`${mouseMemoirs.className} text-2xl bg-Orange h-14 flex items-center justify-center text-white font-semibold`}>
+            @TiketHebat
+        </footer>
+    )
+}
