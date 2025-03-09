ff2af3cc66659a8ebc4eef2625a6f01abbeb9e20
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-03-10 04:59:08 +0700

    feat: add ship info

--- app/(user)/(beranda)/page.tsx
+++ app/(user)/(beranda)/page.tsx
@@ -1,11 +1,13 @@
 import Card from "@/app/ui/beranda/card"
 import Schedule from "@/app/ui/beranda/schedule"
+import InfoShips from "@/app/ui/beranda/infoShips"
 
 export default function Beranda(){
     return(
         <div className="flex flex-col justify-center items-center">
             <Card/>
             <Schedule/>
+            <InfoShips/>
         </div> 
     )
 }
