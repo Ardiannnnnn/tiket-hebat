ba92003f814794f7179b405ff8b0118370ad667f
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-03-11 02:07:35 +0700

    feat: add serach data

--- app/ui/jadwal/jadwal.tsx
+++ app/ui/jadwal/jadwal.tsx
@@ -10,62 +10,53 @@ import {
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 import { poppins } from "@/app/ui/fonts";
-
-const schedules = [
-  {
-    id: 1,
-    route: "Slnabang - Calang",
-    departureDay: "Minggu",
-    departureDate: "01 Desember 2024",
-    departureTime: "17.00 WIB",
-    arrivalDay: "Senin",
-    arrivalDate: "02 Desember 2024",
-    arrivalTime: "08.00 WIB",
-    ship: "Aceh Hebat 1",
-  },
-  {
-    id: 2,
-    route: "Slnabang - Calang",
-    departureDay: "Minggu",
-    departureDate: "01 Desember 2024",
-    departureTime: "17.00 WIB",
-    arrivalDay: "Senin",
-    arrivalDate: "02 Desember 2024",
-    arrivalTime: "08.00 WIB",
-    ship: "Aceh Hebat 1",
-  },
-  {
-    id: 3,
-    route: "Slnabang - Calang",
-    departureDay: "Minggu",
-    departureDate: "01 Desember 2024",
-    departureTime: "17.00 WIB",
-    arrivalDay: "Senin",
-    arrivalDate: "02 Desember 2024",
-    arrivalTime: "08.00 WIB",
-    ship: "Aceh Hebat 1",
-  },
-  {
-    id: 4,
-    route: "Slnabang - Calang",
-    departureDay: "Minggu",
-    departureDate: "01 Desember 2024",
-    departureTime: "17.00 WIB",
-    arrivalDay: "Senin",
-    arrivalDate: "02 Desember 2024",
-    arrivalTime: "08.00 WIB",
-    ship: "Aceh Hebat 1",
-  },
-];
+import { useState, useMemo, useCallback } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import schedules from "@/lib/data/schedule.json";
 
 export default function JadwalKapal() {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [debouncedSearch, setDebouncedSearch] = useState<string>("");
+  
+    const handleSearch = useDebouncedCallback((term: string) => {
+        setDebouncedSearch(term);
+      }, 1000);
+  
+      const filteredSchedules = useMemo(() => {
+        return schedules.filter((schedule) => {
+          const searchLower = debouncedSearch.toLowerCase();
+          return (
+            schedule.route.toLowerCase().includes(searchLower) ||
+            schedule.departureDay.toLowerCase().includes(searchLower) ||
+            schedule.departureDate.toLowerCase().includes(searchLower) ||
+            schedule.departureTime.toLowerCase().includes(searchLower) ||
+            schedule.arrivalDay.toLowerCase().includes(searchLower) ||
+            schedule.arrivalDate.toLowerCase().includes(searchLower) ||
+            schedule.arrivalTime.toLowerCase().includes(searchLower) ||
+            schedule.ship.toLowerCase().includes(searchLower)
+          );
+        });
+      }, [debouncedSearch]);
+
+    const handleSearchChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+          setSearchTerm(e.target.value);
+          handleSearch(e.target.value);
+        },
+        [handleSearch]
+      );
+
+      console.log(filteredSchedules);
+
   return (
-    <div className= {`${poppins.className } md:m-24 m-4 mt-10`}>
+    <div className={`${poppins.className} md:m-24 m-4 mt-10`}>
       <div className="p-4 bg-white border border-Orange rounded-lg">
         <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-8 mt-4">
           <Input
             placeholder="Cari Jadwal..."
-            className="mb-4 md:w-1/3 border-Blue border-2"
+            className="mb-4 md:w-1/3 h-10 border-Blue border-2 shadow-none focus:outline-none"
+            value={searchTerm}
+            onChange={handleSearchChange}
           />
           <h2 className="text-xl font-semibold text-gray-500 text-center">
             Jadwal Keberangkatan Kapal Feri PT ASDP Singkil
@@ -86,7 +77,7 @@ export default function JadwalKapal() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {schedules.map((schedule, index) => (
+            {filteredSchedules.map((schedule, index) => (
               <TableRow key={schedule.id}>
                 <TableCell className="p-4">{index + 1}.</TableCell>
                 <TableCell className="font-semibold">
