52aa9a68c293a784c26b5140c04a4716730f5d77
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-03-16 23:53:47 +0700

    feat: add form penumpang

--- app/ui/form/form.tsx
+++ app/ui/form/form.tsx
@@ -1,16 +1,26 @@
 import CardPrice from "./cardPrice";
-import KendaraanForm from "./formData"
+import KendaraanForm from "./formData";
 import { poppins } from "@/app/ui/fonts";
 
-export default function Form(){
-    return(
-        <div className={`${poppins.className } m-4 flex flex-col-reverse md:flex-row gap-8 justify-center`}>
-            <div className="flex justify-center">
-                <KendaraanForm/>
-            </div>
-            <div className="flex justify-center">
-                <CardPrice/>
-            </div>
+export default function Form() {
+  return (
+    <div className={`${poppins.className} flex flex-col items-center gap-6`}>
+      <div className="mt-6 flex items-center justify-center gap-4">
+        <div className="bg-Blue rounded-full p-2 w-12 text-white font-bold flex justify-center text-2xl">
+          2
         </div>
-    )
+        <p className="text-2xl font-semibold">
+            Isi Data Diri
+        </p>
+      </div>
+      <div className="flex flex-col-reverse md:flex-row gap-4 justify-center">
+        <div className="flex justify-center m-2">
+          <KendaraanForm />
+        </div>
+        <div className="flex justify-center m-2">
+          <CardPrice />
+        </div>
+      </div>
+    </div>
+  );
 }
