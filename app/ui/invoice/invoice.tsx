49a57b43e81e2a6c6f2cb29c613240a33f5769a9
Ardian <ardiannn98@gmail.com>
2025-03-19 02:22:56 +0700

    sizing page book

--- app/ui/invoice/invoice.tsx
+++ app/ui/invoice/invoice.tsx
@@ -20,8 +20,8 @@ const ticketData = {
   jamBerangkat: "17.00 WIB",
   jamSampai: "08.00 WIB",
   kelas: [
-    { nama: "Ekonomi (Dewasa) x1", harga: "Rp78.000", tempat: "E.01" },
-    { nama: "Bisnis (Dewasa) x1", harga: "Rp91.000", tempat: "B.01" },
+    { nama: "Ekonomi x1", harga: "Rp78.000", tempat: "E.01" },
+    { nama: "Bisnis x1", harga: "Rp91.000", tempat: "B.01" },
   ],
   kendaraan: { jenis: "Golongan II", harga: "Rp191.000" },
 };
@@ -64,7 +64,7 @@ export default function Invoice() {
                 <p className="font-semibold">{ticketData.jamSampai}</p>
               </div>
             </div>
-            <div className="text-end md:mr-4">
+            <div className="text-end md:mr-2">
               <div className="mb-2">
                 <p>Asal</p>
                 <p className="font-semibold">{ticketData.asal}</p>
@@ -76,53 +76,68 @@ export default function Invoice() {
             </div>
           </div>
         </CardContent>
-        <CardFooter className={cn("md:flex-row items-start flex-col border-t-2 border-dashed px-0")}>
+        <CardFooter
+          className={cn(
+            "md:flex-row items-start flex-col border-t-2 border-dashed px-0"
+          )}
+        >
+          {/* No. Order */}
           <div className="w-full md:w-fit border-b-2 md:border-none border-dashed items-end p-4 md:items-start md:justify-center flex flex-col">
             <p>No. Order</p>
             <p>c937465758</p>
           </div>
-          <div className="p-4 w-full  space-y-4 md:border-l-2 border-dashed md:p-4">
-            <div>
+
+          {/* Informasi Tiket */}
+          <div className="p-4 w-full space-y-4 md:border-l-2 border-dashed md:p-4">
+            <div className="text-center md:text-start">
               <p>Nama Pemesan</p>
               <p className="font-semibold">{ticketData.pemesan}</p>
             </div>
-            <div className="md:flex md:gap-x-13 space-y-2 md:space-y-0">
+
+            {/* Detail Kelas dan Harga */}
+            <div className="grid text-center md:text-start grid-cols-3 gap-4 space-y-4 md:space-y-0">
               <div>
                 <p>Kelas</p>
                 {ticketData.kelas.map((data, index) => (
-                  <p key={index} className="font-semibold">{data.nama}</p>
-                ))}
-              </div>
-              <div>
-                <p>Harga</p>
-                {ticketData.kelas.map((data, index) => (
-                  <p key={index} className="font-semibold">{data.harga}</p>
+                  <p key={index} className="font-semibold">
+                    {data.nama}
+                  </p>
                 ))}
               </div>
               <div>
                 <p>Tempat</p>
                 {ticketData.kelas.map((data, index) => (
-                  <p key={index} className="font-semibold">{data.tempat}</p>
+                  <p key={index} className="font-semibold">
+                    {data.tempat}
+                  </p>
+                ))}
+              </div>
+              <div>
+                <p>Harga</p>
+                {ticketData.kelas.map((data, index) => (
+                  <p key={index} className="font-semibold">
+                    {data.harga}
+                  </p>
                 ))}
               </div>
             </div>
-            <div className="flex justify-between md:gap-8">
+
+            {/* Kendaraan & Harga */}
+            <div className="text-center md:text-start grid grid-cols-2 md:grid-cols-3 gap-4 items-start">
               <div>
                 <p>Kendaraan</p>
                 <p className="font-semibold">{ticketData.kendaraan.jenis}</p>
               </div>
               <div>
-                <p className="md:text-white">Harga</p>
+                <p>Harga</p>
                 <p className="font-semibold">{ticketData.kendaraan.harga}</p>
               </div>
             </div>
           </div>
+
+          {/* QR Code */}
           <div className="md:ml-12 w-full flex justify-center md:w-fit">
-            <Image
-              src={img}
-              className="w-60"
-              alt="Screenshots of the dashboard project showing desktop version"
-            />
+            <Image src={img} className="w-40 md:w-60" alt="QR Code" />
           </div>
         </CardFooter>
       </Card>
