7558a531c8ebf68aeaae121827cdad2cf216439f
Ardian <ardiannn98@gmail.com>
2025-05-15 19:13:25 +0700

    fix

--- app/(user)/(beranda)/book/[scheduleId]/page.tsx
+++ app/(user)/(beranda)/book/[scheduleId]/page.tsx
@@ -1,6 +1,5 @@
 import Booking from "@/app/ui/booking/book";
 
 export default function BookDetailPage({ params }: { params: { scheduleId: string } }) {
-  const { scheduleId } = params;
-  return <Booking scheduleid={scheduleId} />;
+  return <Booking scheduleid={params.scheduleId} />;
 }
