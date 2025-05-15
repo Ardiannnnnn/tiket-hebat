0f362d34708308dda269a1e185bf6917828e0751
Ardian <ardiannn98@gmail.com>
2025-05-15 20:38:20 +0700

    fix

--- app/(user)/(beranda)/book/[scheduleId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import Booking from "@/app/ui/booking/book";
-
-export default function BookDetailPage({ params }: { params: { scheduleid: string } }) {
-  return <Booking scheduleid={params.scheduleid} />;
-}
--- service/schedule.ts
+++ service/schedule.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import api from "./api";
 import { Schedule, ScheduleResponse } from "@/types/schedule";
 
@@ -9,5 +10,18 @@ export const getSchedule = async (): Promise<ScheduleResponse | null> => {
   } catch (error) {
     console.error("Failed to fetch ships:", error);
     return null; // Return null or handle the error appropriately
+  }
+};
+
+export const getScheduleById = async (
+  id: string | number
+): Promise<Schedule | null> => {
+  try {
+    const response = await api.get<Schedule>(`/schedule/${id}`);
+    console.log("Schedule by ID response", response.data);
+    return response.data;
+  } catch (error) {
+    console.error(`Failed to fetch schedule with id ${id}:`, error);
+    return null;
   }
 };
