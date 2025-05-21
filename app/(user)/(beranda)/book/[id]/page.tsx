48e2e3e97aa6f21cef94db59c82ac7eecb98ca7c
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-05-21 17:18:08 +0700

    fix promise page id book

--- app/(user)/(beranda)/book/[id]/page.tsx
+++ app/(user)/(beranda)/book/[id]/page.tsx
@@ -1,10 +1,11 @@
 import Booking from "@/app/ui/booking/book";
 
-// export default async function BookPage({ params }: { params: Promise<{ id: string }> }) {
-//   const resolvedParams = await params;
+export default async function BookPage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
+  const scheduleid = resolvedParams.id;
 
-export default async function BookPage({ params }: { params: { id: string } }) {
-  const scheduleid = params.id;
+// export default async function BookPage({ params }: { params: { id: string } }) {
+//   const scheduleid = params.id;
 
   const res = await fetch(
     `https://tikethebat.ambitiousflower-0b7495d3.southeastasia.azurecontainerapps.io/api/v1/schedule/${scheduleid}/quota`
