48df7f24b64f4396a2be826cf292dc86b294e6fd
Ardian <ardian_21@mhs.unsyiah.ac.id>
2025-05-22 20:10:34 +0700

    tes endpoint

--- app/ui/login/login.tsx
+++ app/ui/login/login.tsx
@@ -44,8 +44,7 @@ export default function PageLogin() {
       // Ganti ini dengan API login kamu
       const data = await loginUser(values);
         console.log("Login berhasil", data);
-      // await loginUser(values);
-      router.push("/beranda");
+      // await loginUser(values)
     } catch (err) {
       console.error("Login gagal", err);
     } finally {
