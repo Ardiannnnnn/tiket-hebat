07f69fa1bdc0061494465a2c37ba5ad30b908ffd
Ardian <ardiannn98@gmail.com>
2025-05-25 23:56:07 +0700

    fix

--- app/ui/dashboard/editModal.tsx
+++ app/ui/dashboard/editModal.tsx
@@ -18,7 +18,8 @@ import {
 } from "@/components/ui/dialog";
 
 export default function EditKapalPage() {
-  const { id } = useParams();
+   const params = useParams();
+  const id = Array.isArray(params?.id) ? params.id[0] : params?.id;
   const router = useRouter();
   const [kapal, setKapal] = useState<Ship | null>(null);
   const [loading, setLoading] = useState(true);
