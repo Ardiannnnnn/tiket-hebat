7c156282393c05bd83a51e42111bacbe126da14c
Ardian <ardiannn98@gmail.com>
2025-05-25 23:53:41 +0700

    fix update page

--- app/(dashboard)/pelabuhan/edit/[id]/page.tsx
+++ app/(dashboard)/pelabuhan/edit/[id]/page.tsx
@@ -7,7 +7,8 @@ import { useParams } from "next/navigation";
 import { toast } from "sonner";
 
 export default function UpdatePage() {
-  const { id } = useParams();
+ const params = useParams();
+  const id = Array.isArray(params?.id) ? params.id[0] : params?.id;
   const [data, setData] = useState<any>(null);
 
   useEffect(() => {
